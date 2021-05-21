ad87b4e64f6baa87d110e62854214a981ba6dd8c
taimourya <taimourya@gmail.com>
2021-05-21 01:24:10 +0100

    first commit

---
+++ src/main/java/com/example/reservationandlivraisonapi/ReservationAndLivraisonApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationandlivraisonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationAndLivraisonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationAndLivraisonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reservationandlivraisonapi/ReservationAndLivraisonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationandlivraisonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationAndLivraisonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
